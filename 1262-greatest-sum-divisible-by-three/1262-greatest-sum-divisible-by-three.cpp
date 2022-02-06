a090f30992d54c057f99018849691476bf33df3e
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-02-06 16:11:50 +0530

    Time: 148 ms (12.02%), Space: 33.2 MB (31.71%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        
+        return maxSumDivK(nums, 3);
+    }
+    
+    int maxSumDivK(vector<int>& nums, int k) {
         int n = nums.size();
-       vector<vector<int>> dp(n, vector<int>(3));
-   
-        dp[0][nums[0] % 3] = nums[0];
-        for (int i = 1; i < n; i++) 
-        {
-            int num = nums[i];
-            int mod = num % 3;
-            if (mod == 0) {
-                dp[i][0] = dp[i - 1][0] + num;
-                dp[i][1] = dp[i - 1][1] > 0 ? dp[i - 1][1] + num : 0;
-                dp[i][2] = dp[i - 1][2] > 0 ? dp[i - 1][2] + num : 0;
-            } 
-            else if (mod == 1) {
-                dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] > 0 ? dp[i - 1][2] + num : 0);
-                dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + num);
-                dp[i][2] = dp[i - 1][1] > 0 ? dp[i - 1][1] + num : 0;
-            } 
-            else 
-            {    
-                dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] > 0 ? dp[i - 1][1] + num : 0);
-                dp[i][1] = max(dp[i - 1][1], dp[i - 1][2] > 0 ? dp[i - 1][2] + num : 0);
-                dp[i][2] = max(dp[i - 1][2], dp[i - 1][0] + num);
+        vector<int> dp(k, INT_MIN);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            int num = nums[i - 1], mod = nums[i - 1] % k;
+            vector<int> newDp(k);
+            
+            for (int j = 0; j < k; j++) {
+                newDp[j] = max(dp[j], dp[(j + mod) % k] + num);
             }
+            
+            dp = newDp;
         }
-
-        return dp[n - 1][0];
+        
+        return dp[0];
     }
 };
