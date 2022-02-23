c4b61ca23255f016c7abceeafbe5a0177b000ae6
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-02-24 00:33:40 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (98.24%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int dp[59][59];
-    int f(int n, int c)
-    {
-        if(n == 0)
-        {
-            return c > 1;
-        }
-        
-        int& ans = dp[n][c];
-        if(ans == -1)
-            ans = 0;
-        else
-            return ans;
-        for(int i = 1; i <= n; i++)
-        {
-            ans = max(ans, i * f(n - i, c + 1));
-        }
-        return ans;
-    }
     int integerBreak(int n) 
     {
-        memset(dp, -1, sizeof dp);
-        return f(n, 0);    
+        int dp[60] = {0};
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j < i + (i != n); j++)
+            {
+                dp[i] = max(dp[i], dp[i - j] * j);
+            }
+        }
+        return dp[n];    
     }
 };
