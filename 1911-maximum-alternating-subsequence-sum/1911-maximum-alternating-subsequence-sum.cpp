922c1ffc59ead8117f5275d454c58a400974fa6f
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-02-26 11:01:46 +0530

    Time: 203 ms (77.15%), Space: 91.1 MB (68.84%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    long long dp[100001][2];
-    long long f(vector<int>& nums, int ind, int bit)
-    {
-        if(ind >= nums.size())
-            return 0;
-        //take the current num with given parity.
-        if(dp[ind][bit] != -1)
-            return dp[ind][bit];
-        long long ans1 = (bit == 0? (long long) nums[ind]:(long long) (-nums[ind])) + f(nums, ind + 1, 1 - bit);
-        // leave the number..
-        long long ans2 = f(nums, ind + 1, bit); 
-        return dp[ind][bit] = max(ans1, ans2);
-    }
-    long long maxAlternatingSum(vector<int>& nums) {
-        memset(dp, -1, sizeof dp);
-        return f(nums, 0, 0);   
+    
+    long long maxAlternatingSum(vector<int>& A) {
+        long long odd = 0, even = 0;
+        for (int& a: A)
+            even = max(even, odd + a),
+            odd = even - a;
+        return even;   
     }
 };
