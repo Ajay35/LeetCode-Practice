98bcc0fd2d6ec08a6afee3f657a1b5919a510df3
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-02-26 19:22:41 +0530

    Time: 169 ms (73.64%), Space: 91.7 MB (62.40%) - LeetHub

--- 565-array-nesting/565-array-nesting.cpp
+++ 565-array-nesting/565-array-nesting.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int dfs(int num, vector<int>& a, vector<bool>& vis)
-    {
-        if(vis[num])
-            return 0;
-        vis[num] = true;
-        return 1 + dfs(a[num], a, vis);
-    }
+    
     int arrayNesting(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> vis;
-        int ans = 0;
-        vis.resize(n, false);
-        for(int i = 0; i < n; i++)
+        int res = 0;
+        vector<bool> vis(n, false);
+        for (int i = 0; i < n; i++) 
         {
-            if(!vis[i])
-            {
-                ans = max(ans, dfs(nums[i], nums, vis));
+            if (!vis[i]) {
+                int start = nums[i], count = 0;
+                do {
+                    start = nums[start];
+                    count++;
+                    vis[start] = true;
+                }
+                while (start != nums[i]);
+                res = max(res, count);
             }
         }
-        return ans;
+        return res;
     }
 };
