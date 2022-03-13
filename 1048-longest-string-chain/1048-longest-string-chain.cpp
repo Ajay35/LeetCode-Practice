9d6d1264311158c90d4f59e2e097db94962e2dca
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-03-13 13:16:14 +0530

    Time: 78 ms (83.46%), Space: 19.2 MB (52.34%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,36 +1,68 @@
-
 class Solution {
+public:
+    vector<vector<string>> store;
     
-private:
-
-    int dfs(unordered_set<string> &words, unordered_map<string, int> &memo, string currentWord) {
-        if (memo.find(currentWord) != memo.end()) {
-            return memo[currentWord];
-        }
-        int maxLength = 1;
-
-        for (int i = 0; i < currentWord.length(); i++) {
-            string newWord = currentWord.substr(0, i) + currentWord.substr(i + 1);
-            if (words.find(newWord) != words.end()) {
-                int currentLength = 1 + dfs(words, memo, newWord);
-                maxLength = max(maxLength, currentLength);
+    bool ok(string& s, string& t)
+    {
+        int sz1 = s.size();
+        int sz2 = t.size();
+        if(sz1+1 != sz2)
+            return false;
+        int l1 = 0, l2 = 0, cnt = 0;
+        while(l1 < sz1 and l2 < sz2)
+        {
+            if(s[l1] != t[l2])
+            {
+                if(cnt == 1)
+                    return false;
+                l2++;
+                cnt++;
+            }
+            else
+            {
+                l1++;
+                l2++;
             }
         }
-        memo[currentWord] = maxLength;
-
-        return maxLength;
+        if(l2 < t.size())
+            return true;
+        return cnt == 1;
     }
-
-public :
-    int longestStrChain(vector<string> &words) {
-        unordered_map<string, int> memo;
-        unordered_set<string> wordsPresent;
-        for (const string &word : words) {
-            wordsPresent.insert(word);
+    map<string, int> memo;
+    int dfs(string w, int ind)
+    {
+        int ret = 0;
+        if(ind > 16)
+            return 0;
+        if(memo.find(w) != memo.end())
+            return memo[w];
+        for(int i = 0; i < store[ind].size(); i++)
+        {
+            if(ok(w, store[ind][i]))
+            {
+                ret = max(ret, 1 + dfs(store[ind][i], ind + 1));
+            }
         }
-        int ans = 0;
-        for (const string &word : words) {
-            ans = max(ans, dfs(wordsPresent, memo, word));
+        return memo[w] = ret;
+    }
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+        store.resize(17);
+        
+        for(int i = 0; i < n; i++)
+        {
+            int len = words[i].size();
+            store[len].push_back(words[i]);    
+        }
+        int ans = 1;
+        for(int i = 1; i <= 16 ; i++)
+        {
+            for(int j = 0; j < store[i].size(); j++)
+            {
+                string w = store[i][j];
+                int len = 1 + dfs(w, i + 1);
+                ans = max(ans, len);
+            }
         }
         return ans;
     }
