dc9099d03f1435e2995158a8eb1812726cd9e340
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-03-22 19:05:56 +0530

    Time: 264 ms (94.56%), Space: 78.7 MB (72.11%) - LeetHub

---
+++ 2021-brightest-position-on-street/2021-brightest-position-on-street.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int brightestPosition(vector<vector<int>>& lights) 
+    {
+        vector<pair<int, int>> line;
+        int bright = 0, max_bright = 0, res = INT_MIN;
+        
+        for (auto &l : lights) 
+        {
+            line.push_back({l[0] - l[1], 1});
+            line.push_back({l[0] + l[1] + 1, -1});
+        }
+        
+        sort(begin(line), end(line));
+        
+        for (auto [pos, light] : line) 
+        {
+            bright += light;
+            
+            if (bright > max_bright) 
+            {
+                max_bright = bright;
+                res = pos;
+            }
+        }
+        return res;
+    }
+};
