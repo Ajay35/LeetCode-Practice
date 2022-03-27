c2e32b828c6baaf07b3b4c451faf8fe520656e41
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-03-27 11:39:03 +0530

    Time: 4 ms (72.58%), Space: 6.4 MB (86.66%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,28 +1,49 @@
 class Solution {
-    public:
-    int longestSubstring(string s, int k) 
-    {
-        int n = s.size();
-        return longestSubstringUtil(s, 0, n, k);
-    }
-    int longestSubstringUtil(string &s, int start, int end, int k) 
-    {
-        if (end < k) 
-            return 0;
-        int countMap[26] = {0};
-        for (int i = start; i < end; i++)
-            countMap[s[i] - 'a']++;
-        for (int mid = start; mid < end; mid++) 
-        {
-            if (countMap[s[mid] - 'a'] >= k) 
-                continue;
-            int midNext = mid + 1;
-            while (midNext < end && countMap[s[midNext] - 'a'] < k) 
-                midNext++;
-            return max(longestSubstringUtil(s, start, mid, k),
-                    longestSubstringUtil(s, midNext, end, k));
+public:
+    int longestSubstring(string s, int k) {
+        int countMap[26];
+        int maxUnique = getMaxUniqueLetters(s);
+        int result = 0;
+        for (int currUnique = 1; currUnique <= maxUnique; currUnique++) {
+            // reset countMap
+            memset(countMap, 0, sizeof(countMap));
+            int windowStart = 0, windowEnd = 0, idx = 0, unique = 0, countAtLeastK = 0;
+            while (windowEnd < s.size()) {
+                // expand the sliding window
+                if (unique <= currUnique) {
+                    idx = s[windowEnd] - 'a';
+                    if (countMap[idx] == 0) unique++;
+                    countMap[idx]++;
+                    if (countMap[idx] == k) countAtLeastK++;
+                    windowEnd++;
+                }
+                // shrink the sliding window
+                else {
+                    idx = s[windowStart] - 'a';
+                    if (countMap[idx] == k) countAtLeastK--;
+                    countMap[idx]--;
+                    if (countMap[idx] == 0) unique--;
+                    windowStart++;
+                }
+                if (unique == currUnique && unique == countAtLeastK)
+                    result = max(windowEnd - windowStart, result);
+            }
         }
-        return (end - start);
+
+        return result;
+    }
+
+    // get the maximum number of unique letters in the string s
+    int getMaxUniqueLetters(string s) 
+    {
+        bool map[26] = {0};
+        int maxUnique = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!map[s[i] - 'a']) {
+                maxUnique++;
+                map[s[i] - 'a'] = true;
+            }
+        }
+        return maxUnique;
     }
 };
-
