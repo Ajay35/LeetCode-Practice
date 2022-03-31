a413d105e394285331f369ef9ce8236d3d186bd3
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-03-31 11:28:47 +0530

    Time: 25 ms, Memory: 13.6 MB - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.cpp
+++ meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
-    int minMeetingRooms(vector<vector<int>>& interval) {
-        int limit = 0;
-        int n = interval.size();
-        for(int i = 0; i < n; i++)
-        {
-            limit = max(limit, interval[i][1]);
-        }
-        vector<int> a(limit + 1);
-        for(int i = 0; i < n; i++)
-        {
-            int l = interval[i][0];
-            int r = interval[i][1];
-            r--;
-            a[l] += 1;
-            if(r + 1 <= limit)
-                a[r + 1] -= 1;
-        }
-        int ans = 1;
-        for(int i = 1; i <= limit; i++)
-        {
-            a[i] += a[i - 1];
-            ans = max(ans, a[i]);
-        }
-        return ans;
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        map<int, int> m;
+        for (auto &t : intervals)
+            m[t[0]]++, m[t[1]]--;
+        int cur = 0, res = 0;
+        for (auto &it : m)
+            res = max(res, cur += it.second);
+        return res;
     }
 };
