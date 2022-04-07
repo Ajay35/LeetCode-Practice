022f94cfbbf96de7b8dd7503ea3404c73b5dba84
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-04-07 23:52:00 +0530

    Time: 247 ms (80.79%), Space: 97 MB (59.75%) - LeetHub

---
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,35 @@
+class RandomizedSet {
+    vector<int> numberList;
+    unordered_map<int, int> hashTable;
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {}
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if(hashTable.find(val) != hashTable.end())
+        {
+            return false;
+        }
+        hashTable[val] = numberList.size();
+        numberList.push_back(val);
+        return true;
+    }
+    
+    bool remove(int val) 
+    {
+        if(hashTable.find(val) == hashTable.end())
+        {
+            return false;
+        }
+        swap(numberList[hashTable[val]], numberList[numberList.size() - 1]);
+        numberList.pop_back();
+        hashTable[numberList[hashTable[val]]] = hashTable[val];
+        hashTable.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return numberList[rand() % numberList.size()];
+    }
+};
