91793f235daf4617d53104057bf2153ba7501fc4
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-04-10 12:00:10 +0530

    Time: 228 ms (70.64%), Space: 82.9 MB (68.81%) - LeetHub

---
+++ 2031-count-subarrays-with-more-ones-than-zeros/2031-count-subarrays-with-more-ones-than-zeros.cpp
@@ -0,0 +1,53 @@
+class FenwickTree {
+ public:
+  FenwickTree(int n) : n(n), sums(2 * n + 1) {}
+
+  void update(int i, int delta) 
+  {
+    i += n + 1; 
+    while (i < sums.size()) 
+    {
+      sums[i] += delta;
+      i += i & -i;
+    }
+  }
+
+  int get(int i) 
+  {
+    i += n + 1; 
+    int sum = 0;
+    while (i > 0) 
+    {
+      sum += sums[i];
+      i -= i & -i;
+    }
+    return sum;
+  }
+
+ private:
+  const int n;
+  vector<int> sums;
+};
+
+class Solution {
+ public:
+  int subarraysWithMoreZerosThanOnes(vector<int>& nums) 
+  {
+    constexpr int mod = 1e9 + 7;
+
+    int ans = 0;
+    int prefix = 0;
+    FenwickTree tree(nums.size());
+    tree.update(0, 1);
+
+    for (const int num : nums) 
+    {
+      prefix += num == 0 ? -1 : 1;
+      ans += tree.get(prefix - 1);
+      ans %= mod;
+      tree.update(prefix, 1);
+    }
+
+    return ans;
+  }
+};
