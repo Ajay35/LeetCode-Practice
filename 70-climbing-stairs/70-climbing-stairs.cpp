6c790ebe26aa08e5491e11244308482dd2eb6626
Ajay Subhash Jadhav <ajayjadhav35@live.com>
2022-07-05 23:19:23 +0530

    Time: 5 ms (11.67%), Space: 6.3 MB (14.99%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -6,13 +6,15 @@ public:
         if(n <= 1) {
             return n;
         }
-        dp[0] = 1;
-        dp[1] = 1;
+        
+        int a = 1, b = 1;
         
         for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            int temp = b;
+            b = a + b;
+            a = temp;
         }
         
-        return dp[n];
+        return b;
     }
 };
